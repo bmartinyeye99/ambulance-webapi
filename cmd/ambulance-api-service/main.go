f8a1cc51543c501e1dae73046d9147fe81d2b7c2
Martin Bopko <forcemarci@gmail.com>
2025-04-01 10:43:36 +0200

    Kostra servera

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
-    "log"
-    "os"
-    "strings"
-    "github.com/gin-gonic/gin"
-    "github.com/bmartinyeye99/ambulance-webapi/api"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/bmartinyeye99/ambulance-webapi/api"
+	"github.com/bmartinyeye99/ambulance-webapi/internal/ambulance_wl"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    log.Printf("Server started")
-    port := os.Getenv("AMBULANCE_API_PORT")
-    if port == "" {
-        port = "8080"
-    }
-    environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
-    if !strings.EqualFold(environment, "production") { // case insensitive comparison
-        gin.SetMode(gin.DebugMode)
-    }
-    engine := gin.New()
-    engine.Use(gin.Recovery())
-    // request routings
-    engine.GET("/openapi", api.HandleOpenApi)
-    engine.Run(":" + port)
+	log.Printf("Server started")
+	port := os.Getenv("AMBULANCE_API_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
+	if !strings.EqualFold(environment, "production") { // case insensitive comparison
+		gin.SetMode(gin.DebugMode)
+	}
+	engine := gin.New()
+	engine.Use(gin.Recovery())
+
+	handleFunctions := &ambulance_wl.ApiHandleFunctions{
+		AmbulanceConditionsAPI:  ambulance_wl.NewAmbulanceConditionsApi(),
+		AmbulanceWaitingListAPI: ambulance_wl.NewAmbulanceWaitingListApi(),
+	}
+	ambulance_wl.NewRouterWithGinEngine(engine, *handleFunctions)
+
+	// request routings
+	engine.GET("/openapi", api.HandleOpenApi)
+	engine.Run(":" + port)
+
 }
